95db3cd83e35c839e1a36c796e10ce5b9db21c48
dmc <david.mark.clements@gmail.com>
2025-08-14 22:02:07 +0200

    🍐🔳

---
+++ README.md
@@ -0,0 +1,9 @@
+# pear-stamp
+
+> Pear template transforms
+
+## Usage
+
+## License
+
+Apache-2.0
---
+++ test/index.test.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { test } = require('brittle')
+const fs = require('bare-fs')
+const path = require('bare-path')
+
+const stamp = require('..')
+
+const dirname = __dirname
+
+test('stamp sync', async function (t) {
+  t.plan(1)
+
+  const templateFile = path.join(dirname, 'fixtures', 'stamp', 'template.html')
+  const finalFile = path.join(dirname, 'fixtures', 'stamp', 'final.html')
+
+  const template = await fs.promises.readFile(templateFile, 'utf8')
+  const final = await fs.promises.readFile(finalFile, 'utf8')
+
+  const locals = { name: 'world', version: 'v.1.2.3', url: 'https://docs.pears.com/' }
+  const res = stamp.sync(template, locals)
+  t.is(res, final)
+})
+
+test('stamp stream', async function (t) {
+  t.plan(1)
+
+  const templateFile = path.join(dirname, 'fixtures', 'stamp', 'template.html')
+  const finalFile = path.join(dirname, 'fixtures', 'stamp', 'final.html')
+
+  const template = await fs.promises.readFile(templateFile, 'utf8')
+  const final = await fs.promises.readFile(finalFile, 'utf8')
+
+  const locals = { name: 'world', version: 'v.1.2.3', url: 'https://docs.pears.com/' }
+  const stream = stamp.stream(template, locals)
+  t.teardown(() => stream.destroy())
+
+  let res = ''
+  stream.on('data', (chunk) => {
+    res += chunk
+  })
+  await new Promise((resolve) => stream.on('end', resolve))
+
+  t.is(res, final)
+})
